cb6086f51351323f50b4549adba2f123caaafc36
vubare <bare20011215gmail.com>
2021-06-12 18:54:06 +0700

    add all file

---
+++ admin/includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 20px;">Welcome my Admin Panel</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+
+			<li class="ts-account">
+				<a href="#"><img src="https://scontent-hkt1-2.xx.fbcdn.net/v/t1.6435-0/p526x296/124195777_840100296818749_6603636018556676142_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=atVIzPHLuzIAX9Xq3eS&_nc_ht=scontent-hkt1-2.xx&tp=6&oh=f64643fb71caf497ebaf3217c1aff0f5&oe=60E5E9D0" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
